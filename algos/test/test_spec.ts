46f53cc84fafe70522e72fc62dd062a8c99b3621
Grace Tveit <grace.tveit@gmail.com>
2022-03-02 15:46:57 -0800

    resetting repo

--- algos/test/test_spec.ts
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-import helloWorld from './test';
-
-describe('Test', () => {
-  it('should return "hello world"', () => {
-    assert(helloWorld() === 'hello world');
-  });
-});
